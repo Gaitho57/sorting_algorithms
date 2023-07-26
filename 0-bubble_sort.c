57bbf763e9c2a0231cdc1309e48018ea063a0051
Gaitho57 <michaelgaitho10@gmail.com>
2023-07-26 02:03:04 -0700

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,47 +1,38 @@
 #include "sort.h"
-#include <stdbool.h>
 
 /**
- * swap_ints - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
- */
-static void swap_ints(int *a, int *b)
-{
-int tmp;
-
-tmp = *a;
-*a = *b;
-*b = tmp;
-}
-
-/**
- * bubble_sort - Sort an array of integers in ascending order.
- * @array: An array of integers to sort.
- * @size: The size of the array.
- *
- * Description: Prints the array after each swap.
+ * bubble_sort - function which sorts the array using bubble sort
+ * @array: the array to sort
+ * @size: the size of the array
  */
 void bubble_sort(int *array, size_t size)
 {
-size_t i, len = size;
-bool bubbly = false;
+	size_t i, j, optimize = size - 1;
+	int swap = 0, temp;
 
-if (array == NULL || size < 2)
-return;
-
-while (bubbly == false)
-{
-bubbly = true;
-for (i = 0; i < len - 1; i++)
-{
-if (array[i] > array[i + 1])
-{
-swap_ints(array + i, array + i + 1);
-print_array(array, size);
-bubbly = false;
-}
-}
-len--;
-}
+	/* when empty or contains only one element */
+	if (size < 2)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		/*
+		 * enhances the variable used to save on time
+		 * everytime the largest object appears on the far most right
+		 */
+		for (j = 0; j < optimize; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap = 1;
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+		if (swap == 0)
+			return;
+		swap = 0;
+		optimize--;
+	}
 }
