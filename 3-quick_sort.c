b15d1b98cffe76a044a6977786d144ee6ae8cb23
Gaitho57 <michaelgaitho10@gmail.com>
2023-07-25 18:28:40 -0700

    quick

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,27 +1,22 @@
 #include "sort.h"
 
-void swap_ints(int *a, int *b);
-int lomuto_partition(int *array, size_t size, int left, int right);
-void lomuto_sort(int *array, size_t size, int left, int right);
-void quick_sort(int *array, size_t size);
-
 /**
  * swap_ints - Swap two integers in an array.
  * @a: The first integer to swap.
  * @b: The second integer to swap.
  */
-void swap_ints(int *a, int *b)
+static void swap_ints(int *a, int *b)
 {
-	int tmp;
+int tmp;
 
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
+tmp = *a;
+*a = *b;
+*b = tmp;
 }
 
 /**
  * lomuto_partition - Order a subset of an array of integers according to
- *                    the lomuto partition scheme (last element as pivot).
+ *                    the Lomuto partition scheme (last element as pivot).
  * @array: The array of integers.
  * @size: The size of the array.
  * @left: The starting index of the subset to order.
@@ -31,29 +26,29 @@ void swap_ints(int *a, int *b)
  */
 int lomuto_partition(int *array, size_t size, int left, int right)
 {
-	int *driver, up, down;
+int *driver, up, down;
 
-	driver = array + right;
-	for (up = down = left; down < right; down++)
-	{
-		if (array[down] < *driver)
-		{
-			if (up < down)
-			{
-				swap_ints(array + down, array + up);
-				print_array(array, size);
-			}
-			up++;
-		}
-	}
+driver = array + right;
+for (up = down = left; down < right; down++)
+{
+if (array[down] < *driver)
+{
+if (up < down)
+{
+swap_ints(array + down, array + up);
+print_array(array, size);
+}
+up++;
+}
+}
 
-	if (array[up] > *driver)
-	{
-		swap_ints(array + up, driver);
-		print_array(array, size);
-	}
+if (array[up] > *driver)
+{
+swap_ints(array + up, driver);
+print_array(array, size);
+}
 
-	return (up);
+return (up);
 }
 
 /**
@@ -67,14 +62,14 @@ int lomuto_partition(int *array, size_t size, int left, int right)
  */
 void lomuto_sort(int *array, size_t size, int left, int right)
 {
-	int belong;
+int belong;
 
-	if (right - left > 0)
-	{
-		belong = lomuto_partition(array, size, left, right);
-		lomuto_sort(array, size, left, belong - 1);
-		lomuto_sort(array, size, belong + 1, right);
-	}
+if (right - left > 0)
+{
+belong = lomuto_partition(array, size, left, right);
+lomuto_sort(array, size, left, belong - 1);
+lomuto_sort(array, size, belong + 1, right);
+}
 }
 
 /**
@@ -83,13 +78,18 @@ void lomuto_sort(int *array, size_t size, int left, int right)
  * @array: An array of integers.
  * @size: The size of the array.
  *
- * Description: Uses the Lomuto partition scheme. Prints
- *              the array after each swap of two elements.
+ * Description: Uses the Lomuto partition scheme.
+ *              Prints the array after each swap of two elements.
  */
 void quick_sort(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
-		return;
+if (array == NULL || size < 2)
+return;
 
-	lomuto_sort(array, size, 0, size - 1);
+/* Shuffle the array to improve average case */
+/* You can use a separate function for shuffling or a Fisher-Yates shuffle */
+/* For simplicity, we won't include shuffling here */
+
+lomuto_sort(array, size, 0, size - 1);
 }
+
